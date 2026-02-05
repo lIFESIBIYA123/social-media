07d9d09f2967f2ceada76d8767ae92236260a402
lIFESIBIYA123 <lifesibiya221@gmail.com>
2026-02-05 12:51:17 +0200

    dockerize

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -10,25 +10,33 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV = os.environ.get("ENV")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%##aecv2(=lj8$^4jnhr7kzzfku68o#nnv@@q(ib9l$8q2_a=p'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 
+    default='django-insecure-%##aecv2(=lj8$^4jnhr7kzzfku68o#nnv@@q(ib9l$8q2_a=p'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if ENV == "PROD" else True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['54.82.138.3', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="54.82.138.3,localhost,127.0.0.1,*").split(",")
 
 AUTH_USER_MODEL = 'core_user.User' # tells django to use our models
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,7 +52,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     
     'corsheaders',
-
 
     'core',
     'core.user',
@@ -70,8 +77,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Must be first
-    'django.middleware.common.CommonMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -101,21 +106,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CoreRoot.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coredb',
-        'USER': 'core',
-        'PASSWORD': 'Gershom@sibiya123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME", "coredb"),
+        "USER": os.getenv("DATABASE_USER", "core"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "Gershom@sibiya123"),
+        "HOST": os.environ.get("DATABASE_HOST", "db"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -135,7 +138,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -146,7 +148,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- dockerfile
+++ dockerfile
@@ -1,18 +1,23 @@
-# Base image
-FROM python:3.10.4-slim-bullseye
+# pull official base image
+FROM python:3.10-alpine
 
-# Environment variables
+# set work directory
+WORKDIR /app
+
+# set environment variables
 ENV PIP_DISABLE_PIP_VERSION_CHECK=1
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
-# Set working directory
-WORKDIR /code
+# install psycopg2 dependencies
+RUN apk update \
+   && apk add --no-cache postgresql-dev gcc musl-dev jpeg-dev zlib-dev
 
-# Install dependencies
+# install python dependencies
 COPY requirements.txt .
 RUN pip install --upgrade pip \
-    && pip install --default-timeout=100 --retries=10 -r requirements.txt
+    && pip install -U setuptools \
+    && pip install --default-timeout=100 --retries=10 --no-cache-dir -r requirements.txt
 
-# Copy project files
+# copy project
 COPY . .
